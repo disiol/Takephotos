67914b1d61965beda9a1447cbb1e6da6c4d194d6
den <d>
2018-08-27 18:36:01 +0300

    I made a function createImageFile

--- app/src/main/java/denisimus_it/net/takephotos/MainActivity.java
+++ app/src/main/java/denisimus_it/net/takephotos/MainActivity.java
@@ -1,13 +1,18 @@
 package denisimus_it.net.takephotos;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,10 +20,13 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
     private ImageView imageViewFoto;
     private Button buttonTakeFoto;
 
     String mCurrentPhotoPath;
+    private Uri photoURI;
 
 
     @Override
@@ -26,29 +34,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageViewFoto = findViewById(R.id.imageViewFoto);
-        buttonTakeFoto= findViewById(R.id.buttonTakeFoto);
+        buttonTakeFoto = findViewById(R.id.buttonTakeFoto);
 
+        buttonTakeFoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO
+                dispatchTakePictureIntent();
+
+            }
+        });
 
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            imageViewFoto.setImageURI(photoURI);
         }
     }
 
+
     private File createImageFile() throws IOException {
         // Create an image file name
+        String timeStamp = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "_";
+        }
+
         String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName,  ".jpg",storageDir);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
     }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
+//                ex.getCause();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                 photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+
+
+    }
+
 
 }
