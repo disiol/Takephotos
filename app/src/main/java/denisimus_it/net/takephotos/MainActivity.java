e6cbc31ba46ba0720f1fd9ad7adffd0d653f7e57
den <d>
2018-08-27 17:50:06 +0300

    I made a function createImageFile

--- app/src/main/java/denisimus_it/net/takephotos/MainActivity.java
+++ app/src/main/java/denisimus_it/net/takephotos/MainActivity.java
@@ -1,16 +1,25 @@
 package denisimus_it.net.takephotos;
 
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageViewFoto;
     private Button buttonTakeFoto;
+
+    String mCurrentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,4 +39,16 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,  ".jpg",storageDir);
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
