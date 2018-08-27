4cb29e21e64fbd730c1aca724cfdadcdd2cbade6
den <d>
2018-08-27 17:40:03 +0300

    I made a project skeleton

--- app/src/main/java/denisimus_it/net/takephotos/MainActivity.java
+++ app/src/main/java/denisimus_it/net/takephotos/MainActivity.java
@@ -4,16 +4,20 @@ import android.content.Intent;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageViewFoto;
+    private Button buttonTakeFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageViewFoto = findViewById(R.id.imageViewFoto);
+        buttonTakeFoto= findViewById(R.id.buttonTakeFoto);
 
 
 
